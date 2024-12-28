059d4cb4f02da379315bb34fae06011d880e136a
Bilal-Zaro <bilalza2004@gmail.com>
2024-12-28 19:52:57 +0300

    nmnm

---
+++ task.js
@@ -0,0 +1,28 @@
+console.log(-5 * 3);
+console.log("JavaScript" + 50);
+console.log(17 % 5);
+console.log(5 % 17);
+console.log(5 / 10);
+console.log(4 === "4");
+console.log(4 != 5);
+console.log(7 <= 8);
+console.log("Hello" + 5);
+let x = 3.5;
+console.log(Math.ceil(x) - Math.floor(x));
+console.log(Math.pow(x, 2));
+/************/
+
+let a = prompt("Enter any number : ");
+alert("The number is : " + a);
+let b = parseFloat(prompt("Enter two numbers and I will display their sum\n The first number : "));
+let c = parseFloat(prompt("The second number : "));
+alert(b + c);
+let d = parseFloat(prompt("Enter two numbers and I will display their manubilation\n The first number : "));
+let e = parseFloat(prompt("The second number : "));
+alert(d*e);
+let f = parseFloat(prompt("Enter two numbers and I will display their dividing\n The first number : "));
+let g = parseFloat(prompt("The second number : "));
+alert(f/g);
+let n =2;
+let m=3;
+alert(n+m);
